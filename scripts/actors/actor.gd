2782676934e2ae8a50fe5c3a4269d5ef423a1c19
Matsukari <divineswordbearer@gmail.com>
2025-05-20 16:42:29 +0800

    Fixed wall bug collision animation loop

--- scripts/actors/actor.gd
+++ scripts/actors/actor.gd
@@ -49,10 +49,10 @@ func afk_behaviour(delta: float):
 		velocity.x = move_toward(velocity.x, sign(velocity.x) * speed, accel * speed * delta)
 	elif dist.x > follow_distance || dist.x < -follow_distance:
 		velocity.x = move_toward(velocity.x, sign(dist.x) * speed, accel * speed * delta)
-		if $CliffDetector.is_colliding():
-			try_jump()
 	else:
 		velocity.x = move_toward(velocity.x, 0, accel * speed * delta)
+	if $CliffDetector.is_colliding():
+		try_jump()
 	
 
 func _physics_process(delta: float) -> void:
@@ -67,6 +67,7 @@ func _physics_process(delta: float) -> void:
 	if is_player:
 		var speed = walk_speed
 		var accel = walk_accel
+		
 		if Input.is_action_pressed("run"):
 			speed = run_speed
 			accel = run_accel
@@ -93,18 +94,24 @@ func _physics_process(delta: float) -> void:
 func get_new_animation() -> String:
 	var animation_new: String
 	if is_on_floor():
-		if absf(velocity.x) > 0.1:
-			animation_new = "walk"
-			if Input.is_action_pressed("run"):
-				animation_new = "run"
+		if is_player:
+			if Input.is_action_pressed("move_left") or Input.is_action_pressed("move_right"):
+				animation_new = "walk"
+			else:
+				animation_new = "idle"
 		else:
-			animation_new = "idle"
+			if absf(velocity.x) > 0.1:
+				animation_new = "walk"
+			else:
+				animation_new = "idle"
 		is_jumping = false
 	else:
 		if velocity.y > 0.0:
 			animation_new = "fall"
 		else:
 			animation_new = "jump"
+	if Input.is_action_pressed("run") and animation_new == "walk":
+		animation_new = "run"
 	return animation_new
 
 
